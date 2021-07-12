e8695c37c4d62cdd654f1a845f71747baa45dc39
Gegam Arakelyan <gexam96@MBP-Gegam.Dlink>
2021-07-13 01:34:14 +0400

    mongoose connection

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { createServer } = require('http');
+const app = express();
+
+
+mongoose.connect('mongodb+srv://Gegham:g13131213@cluster0.8hk1c.mongodb.net/firstMongo?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true
+    },
+)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err))
+
+const server = createServer(app);
+server.listen(3000, () => console.log('Server connected'));
